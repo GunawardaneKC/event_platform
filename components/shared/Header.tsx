9740b538f16d59c95f34ff73df11cf9e86b453dc
Chamod S.G.K <kalanachamodac@gmail.com>
2025-02-04 21:54:56 +0530

    logo and description page updated

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -11,7 +11,7 @@ const Header = () => {
       <div className="wrapper flex items-center justify-between">
         <Link href="/" className="w-36">
           <Image 
-            src="/assets/images/logo.svg" width={128} height={38}
+            src="/assets/images/logo3.png" width={148} height={38}
             alt="Evently logo" 
           />
         </Link>
