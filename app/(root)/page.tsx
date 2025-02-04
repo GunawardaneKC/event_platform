b47855729538cd9c0700f11c3c695aa1c91e2ec3
Gunawardane K.C. <kavinduchanditha@gmail.com>
2025-02-04 18:30:36 +0530

    Banner update

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -24,8 +24,8 @@ export default async function Home({ searchParams }: SearchParamProps) {
       <section className="bg-primary-50 bg-dotted-pattern bg-contain py-5 md:py-10">
         <div className="wrapper grid grid-cols-1 gap-5 md:grid-cols-2 2xl:gap-0">
           <div className="flex flex-col justify-center gap-8">
-            <h1 className="h1-bold">Host, Connect, Celebrate: Your Events, Our Platform!</h1>
-            <p className="p-regular-20 md:p-regular-24">Book and learn helpful tips from 3,168+ mentors in world-class companies with our global community.</p>
+            <h1 className="h1-bold">Get Ready for the Show of a Lifetime!</h1>
+            <p className="p-regular-20 md:p-regular-24">Discover and book tickets for the hottest musical concerts and live events. Never miss out on the music that moves you!</p>
             <Button size="lg" asChild className="button w-full sm:w-fit">
               <Link href="#events">
                 Explore Now
